eacd1694fdfebabbdb47c580d02de061827301f3
qwe1234k <bbyl0124@gmail.com>
2022-05-27 23:38:12 +0900

    7

--- app.js
+++ app.js
@@ -4,10 +4,15 @@ const http = require("http");
 const { Server } = require("socket.io");
 const cors = require("cors");
 
+app.use(cors());
+
 const server = http.createServer(app);
 
 const io = new Server(server, {
   path: "/socket.io",
+  cors: {
+    origin: "http://localhost:3001",
+  },
 });
 
 io.on("connection", (socket) => {
