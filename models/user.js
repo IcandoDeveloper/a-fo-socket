4a25121e3a4a4ded6e530e22d3e5dd1e647ddc52
qwe1234k <bbyl0124@gmail.com>
2022-05-28 00:38:12 +0900

    9

---
+++ models/user.js
@@ -0,0 +1,29 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {}
+  }
+  User.init(
+    {
+      userId: {
+        primaryKey: true,
+        type: DataTypes.STRING,
+      },
+      userName: DataTypes.STRING,
+      userImageUrl: DataTypes.STRING,
+      penalty: DataTypes.INTEGER,
+      penaltedAt: DataTypes.DATE,
+    },
+    {
+      sequelize,
+      modelName: "User",
+    }
+  );
+  return User;
+};
