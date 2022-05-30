4eaf54f84a2b680a3653b2b8a7f7e43f6b53bfef
qwe1234k <bbyl0124@gmail.com>
2022-05-30 15:13:09 +0900

    주석 수정

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ const io = new Server(server, {
 // 목적: A와 B가 있을때,
 // ## 1. B-->A DM을 보낸다.[ B는 RoomId(A_B)라는 채팅방을 만들고 입장한다. -->  B-->A DM을 보낸다.(대화내용을 DB에 저장) ]
 // ## 2. A는 상단에 메세지버튼을 누르면 RoomId(A_B)가지고 있는 채팅방 목록이 뜬다.
-// 3. A가 Room을 누르면 RoomId(A_B)라는 채팅방에 입장을 한다. [ A가 RoomId(A_B)에 입장한다. --> 대화내용을 DB에서 불러온다. ]
+// ## 3. A가 Room을 누르면 RoomId(A_B)라는 채팅방에 입장을 한다. [ A가 RoomId(A_B)에 입장한다. --> 대화내용을 DB에서 불러온다. ]
 
 io.on("connection", (socket) => {
   // social = { "socketId" : `${socket.id}`,
